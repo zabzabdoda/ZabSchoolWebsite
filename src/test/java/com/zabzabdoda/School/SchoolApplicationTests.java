e01653a37cb8259e37bd7f26fca437cbbe9e79d8
zabzabdoda <tjm761@comcast.net>
2022-06-06 19:23:08 -0700

    Initial commit

---
+++ src/test/java/com/zabzabdoda/School/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zabzabdoda.School;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
